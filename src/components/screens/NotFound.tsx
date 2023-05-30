a2a0b40b025a3326c3b2c273d8469552b2942714
Your Name <you@example.com>
2023-05-31 01:07:57 +0300

    udpated

--- src/components/screens/NotFound.tsx
+++ src/components/screens/NotFound.tsx
@@ -1,9 +1,9 @@
-// import Head from '~/components/shared/Head';
+import Head from '~/components/shared/Head';
 
 export default function NotFoundScreen() {
   return (
     <>
-      {/* <Head title="Page not found" /> */}
+      <Head title="Page not found" />
       <div className="hero flex-grow">
         <div className="hero-content text-center">
           <div className="max-w-md">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{tsx,ts}'],
+/** @type {import('tailwindcss').Config} */
+module.exports =  {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    extend: {},
   },
-  plugins: [require('daisyui')],
-};
+  plugins: [],
+}
